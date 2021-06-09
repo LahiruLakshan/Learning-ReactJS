fcc8b5eb28a27d71eb3a9dd537e010347ca19ce4
Lahiru Lakshan <balahirulakshan@gmail.com>
2021-06-09 17:55:51 +0530

    Updated home page blog box

--- src/home.js
+++ src/home.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Home = (props) => {
+    const [blogs, setBBlogs] = useState([
+        {title: "My new Website", body: "lorem ipsum...", author: "lakshan", id: 1},
+        {title: "Welcome party!", body: "lorem ipsum...", author: "lahiru", id: 2},
+        {title: "Web dev top tip", body: "lorem ipsum...", author: "stark", id: 3}
+    ]);
     const handleClick = () => {
         console.log("hello, lahiru");
-        }
+    }
 
 
     return (
-        <div>
-            <h1>Home Page</h1>
-            <button onClick={handleClick}>Click me</button>
+        <div className={"home"}>
+
+            {blogs.map((blog) => (
+                <div className={"blog-preview"} key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
+
+            {/*<h2>Home Page</h2>*/}
+            {/*<button onClick={handleClick}>Click me</button>*/}
         </div>
     );
 }
