4d5bf28703975ade979f44dd39bf83651c32ce2a
Lahiru Lakshan <balahirulakshan@gmail.com>
2021-06-09 21:47:00 +0530

    Added router and Created the create.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 import Navbar from "./navbar";
 import Home from "./home";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-        <Navbar/>
-      <div className={"content"}>
-          <Home/>
-      </div>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Navbar/>
+                <div className={"content"}>
+                    <Switch>
+                        <Route path={"/"}><Home/></Route>
+                        
+                    </Switch>
+                </div>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/create.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Create(props) {
+    return (
+        <div>
+            <h2>Add a new blog</h2>
+        </div>
+    );
+}
+
+export default Create;
