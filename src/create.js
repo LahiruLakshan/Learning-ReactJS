5dd7b235ba2240810b5057217c5a8ec3570bb634
Lahiru Lakshan <balahirulakshan@gmail.com>
2021-06-12 17:03:33 +0530

    Now we can create new blogs and added the home page

--- src/create.js
+++ src/create.js
@@ -3,20 +3,41 @@ import React, {useState} from 'react';
 function Create(props) {
 
     const [title, setTitle] = useState();
+    const [body, setBody] = useState();
+    const [author, setAuthor] = useState("lahiru");
+    const [isPending, setIsPending] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = {title, body, author};
+        setIsPending(true);
+        fetch("http://localhost:8000/blogs",{
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body: JSON.stringify(blog)
+        }).then(() =>{
+            setIsPending(false);
+        })
+    }
+
     return (
         <div className={"create"}>
             <h2>Add a new blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog Title:</label>
                 <input type={"text"} value={title} onChange={(e) => setTitle(e.target.value)} required/>
                 <label>Blog Body:</label>
-                <textarea required></textarea>
+                <textarea value={body} onChange={(e) => setBody(e.target.value)}  required></textarea>
                 <label>Blog author:</label>
-                <select>
+                <select value={author} onChange={(e) => setAuthor(e.target.value)} >
                     <option value={"lahiru"}>lahiru</option>
+                    <option value={"kusal"}>kusal</option>
+                    <option value={"dimuth"}>dimuth</option>
+                    <option value={"kalana"}>kalana</option>
                     <option value={"Tissera"}>Tissera</option>
                 </select>
-                <button>Add Blogs</button>
+                { !isPending && <button>Add Blogs</button>}
+                { isPending && <button disabled>Adding Blog...</button>}
             </form>
         </div>
     );
