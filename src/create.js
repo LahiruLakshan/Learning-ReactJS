bb4c7fd5bfbfd74c4cd4de3933e109d60156b457
Lahiru Lakshan <balahirulakshan@gmail.com>
2021-06-12 15:03:36 +0530

    Controlled Inputs added to create page

--- src/create.js
+++ src/create.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Create(props) {
+
+    const [title, setTitle] = useState();
     return (
-        <div>
+        <div className={"create"}>
             <h2>Add a new blog</h2>
+            <form>
+                <label>Blog Title:</label>
+                <input type={"text"} value={title} onChange={(e) => setTitle(e.target.value)} required/>
+                <label>Blog Body:</label>
+                <textarea required></textarea>
+                <label>Blog author:</label>
+                <select>
+                    <option value={"lahiru"}>lahiru</option>
+                    <option value={"Tissera"}>Tissera</option>
+                </select>
+                <button>Add Blogs</button>
+            </form>
         </div>
     );
 }
